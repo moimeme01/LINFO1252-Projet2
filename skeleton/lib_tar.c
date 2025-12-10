3dc258b9a9d53d53a7f9335ab3d8fd32e03ab2fa
Thibault Vanni <thibault.vanni@student.uclouvain.be>
2025-12-10 11:53:57 +0100

    forst commit

---
+++ skeleton/lib_tar.c
@@ -0,0 +1,124 @@
+#include "lib_tar.h"
+
+/**
+ * Checks whether the archive is valid.
+ *
+ * Each non-null header of a valid archive has:
+ *  - a magic value of "ustar" and a null,
+ *  - a version value of "00" and no null,
+ *  - a correct checksum
+ *
+ * @param tar_fd A file descriptor pointing to the start of a file supposed to contain a tar archive.
+ *
+ * @return a zero or positive value if the archive is valid, representing the number of non-null headers in the archive,
+ *         -1 if the archive contains a header with an invalid magic value,
+ *         -2 if the archive contains a header with an invalid version value,
+ *         -3 if the archive contains a header with an invalid checksum value
+ */
+int check_archive(int tar_fd) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Checks whether an entry exists in the archive.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive,
+ *         any other value otherwise.
+ */
+int exists(int tar_fd, char *path) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Checks whether an entry exists in the archive and is a directory.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive or the entry is not a directory,
+ *         any other value otherwise.
+ */
+int is_dir(int tar_fd, char *path) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Checks whether an entry exists in the archive and is a file.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive or the entry is not a file,
+ *         any other value otherwise.
+ */
+int is_file(int tar_fd, char *path) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Checks whether an entry exists in the archive and is a symlink.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ * @return zero if no entry at the given path exists in the archive or the entry is not symlink,
+ *         any other value otherwise.
+ */
+int is_symlink(int tar_fd, char *path) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Lists the entries at a given path in the archive.
+ * list() does *not* recurse into the directories listed at the given path.
+ * If the path is NULL, it lists the entries at the root of the archive.
+ *
+ * Example:
+ *  dir/          list(..., "dir/", ...) lists "dir/a", "dir/b", "dir/c/" and "dir/e/"
+ *   ├── a
+ *   ├── b
+ *   ├── c/
+ *   │   └── d
+ *   └── e/
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive. If the entry is a symlink, it must be resolved to its linked-to entry.
+ * @param entries An array of char arrays, each one is long enough to contain a tar entry path.
+ * @param no_entries An in-out argument.
+ *                   The caller set it to the number of entries in `entries`.
+ *                   The callee set it to the number of entries listed.
+ *
+ * @return zero if no directory at the given path exists in the archive,
+ *         1 in case of success,
+ *         -1 in case of error.
+ */
+int list(int tar_fd, char *path, char **entries, size_t *no_entries) {
+    // TODO
+    return 0;
+}
+
+/**
+ * Adds a file at the end of the archive, at the archive's root level.
+ * The archive's metadata must be updated accordingly.
+ * For the file header, only the name, size, typeflag, magic value (to "ustar"), version value (to "00") and checksum fields need to be correctly set.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param filename The name of the file to add. If an entry already exists with the same name, the file is not written, and the function returns -1.
+ * @param src A source buffer containing the file content to add.
+ * @param len The length of the source buffer.
+ *
+ * @return 0 if the file was added successfully,
+ *         -1 if the archive already contains an entry at the given path,
+ *         -2 if an error occurred
+ */
+int add_file(int tar_fd, char *filename, uint8_t *src, size_t len) {
+    // TODO
+    return 0;
+}
---
+++ skeleton/lib_tar.h
@@ -0,0 +1,147 @@
+#ifndef LIB_TAR_H
+#define LIB_TAR_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <unistd.h>
+
+typedef struct posix_header
+{                              /* byte offset */
+    char name[100];               /*   0 */
+    char mode[8];                 /* 100 */
+    char uid[8];                  /* 108 */
+    char gid[8];                  /* 116 */
+    char size[12];                /* 124 */
+    char mtime[12];               /* 136 */
+    char chksum[8];               /* 148 */
+    char typeflag;                /* 156 */
+    char linkname[100];           /* 157 */
+    char magic[6];                /* 257 */
+    char version[2];              /* 263 */
+    char uname[32];               /* 265 */
+    char gname[32];               /* 297 */
+    char devmajor[8];             /* 329 */
+    char devminor[8];             /* 337 */
+    char prefix[155];             /* 345 */
+    char padding[12];             /* 500 */
+} tar_header_t;
+
+#define TMAGIC   "ustar"        /* ustar and a null */
+#define TMAGLEN  6
+#define TVERSION "00"           /* 00 and no null */
+#define TVERSLEN 2
+
+/* Values used in typeflag field.  */
+#define REGTYPE  '0'            /* regular file */
+#define AREGTYPE '\0'           /* regular file */
+#define LNKTYPE  '1'            /* link */
+#define SYMTYPE  '2'            /* reserved */
+#define DIRTYPE  '5'            /* directory */
+
+/* Converts an ASCII-encoded octal-based number into a regular integer */
+#define TAR_INT(char_ptr) strtol(char_ptr, NULL, 8)
+
+/**
+ * Checks whether the archive is valid.
+ *
+ * Each non-null header of a valid archive has:
+ *  - a magic value of "ustar" and a null,
+ *  - a version value of "00" and no null,
+ *  - a correct checksum
+ *
+ * @param tar_fd A file descriptor pointing to the start of a file supposed to contain a tar archive.
+ *
+ * @return a zero or positive value if the archive is valid, representing the number of non-null headers in the archive,
+ *         -1 if the archive contains a header with an invalid magic value,
+ *         -2 if the archive contains a header with an invalid version value,
+ *         -3 if the archive contains a header with an invalid checksum value
+ */
+int check_archive(int tar_fd);
+
+/**
+ * Checks whether an entry exists in the archive.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive,
+ *         any other value otherwise.
+ */
+int exists(int tar_fd, char *path);
+
+/**
+ * Checks whether an entry exists in the archive and is a directory.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive or the entry is not a directory,
+ *         any other value otherwise.
+ */
+int is_dir(int tar_fd, char *path);
+
+/**
+ * Checks whether an entry exists in the archive and is a file.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive or the entry is not a file,
+ *         any other value otherwise.
+ */
+int is_file(int tar_fd, char *path);
+
+/**
+ * Checks whether an entry exists in the archive and is a symlink.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive.
+ *
+ * @return zero if no entry at the given path exists in the archive or the entry is not symlink,
+ *         any other value otherwise.
+ */
+int is_symlink(int tar_fd, char *path);
+
+/**
+ * Lists the entries at a given path in the archive.
+ * list() does *not* recurse into the directories listed at the given path.
+ * If the path is NULL, it lists the entries at the root of the archive.
+ *
+ * Example:
+ *  dir/          list(..., "dir/", ...) lists "dir/a", "dir/b", "dir/c/" and "dir/e/"
+ *   ├── a
+ *   ├── b
+ *   ├── c/
+ *   │   └── d
+ *   └── e/
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param path A path to an entry in the archive. If the entry is a symlink, it must be resolved to its linked-to entry.
+ * @param entries An array of char arrays, each one is long enough to contain a tar entry path.
+ * @param no_entries An in-out argument.
+ *                   The caller set it to the number of entries in `entries`.
+ *                   The callee set it to the number of entries listed.
+ *
+ * @return zero if no directory at the given path exists in the archive,
+ *         1 in case of success,
+ *         -1 in case of error.
+ */
+int list(int tar_fd, char *path, char **entries, size_t *no_entries);
+
+/**
+ * Adds a file at the end of the archive, at the archive's root level.
+ * The archive's metadata must be updated accordingly.
+ * For the file header, only the name, size, typeflag, magic value (to "ustar"), version value (to "00") and checksum fields need to be correctly set.
+ *
+ * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
+ * @param filename The name of the file to add. If an entry already exists with the same name, the file is not written, and the function returns -1.
+ * @param src A source buffer containing the file content to add.
+ * @param len The length of the source buffer.
+ *
+ * @return 0 if the file was added successfully,
+ *         -1 if the archive already contains an entry at the given path,
+ *         -2 if an error occurred
+ */
+int add_file(int tar_fd, char *filename, uint8_t *src, size_t len);
+
+#endif
